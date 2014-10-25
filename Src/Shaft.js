40e12f521edafded5e3419611fd4b71319d6ff71
Casey Clyde <caseysclyde@gmail.com>
2014-10-25 13:41:54 -0700

    basic phyics and energy

--- Src/Shaft.js
+++ Src/Shaft.js
@@ -16,7 +16,7 @@ Shaft.create = function() {
     game.add.plugin(Phaser.Plugin.Debug);
 
 	game.physics.startSystem(Phaser.Physics.P2JS);
-    game.physics.p2.gravity.y = 500;
+    game.physics.p2.gravity.y = 2000;
     //game.time.deltaCap = 0.016;
 
     player = game.add.sprite(100, 100, 'player');
@@ -25,8 +25,21 @@ Shaft.create = function() {
     player.body.x = game.width / 2;
     player.body.debug = true;
 
+    this.clickSpot = {};
     this.clickSpot.x = 0;
     this.clickSpot.y = 0;
+
+    this.energy = 100;
+    this.energyTicker = 0;
+
+    this.hooked = false;
+    this.connectionBroke = false;
+
+    game.input.mouse.mouseDownCallback = function(event) {
+        Shaft.clickSpot.x = game.input.x;
+        Shaft.clickSpot.y = game.input.y;
+        Shaft.connectionBroke = false;
+    };
 
     /*
     //  Make things a bit more bouncey
@@ -39,10 +52,53 @@ Shaft.create = function() {
     sprite.body.setZeroDamping();
     sprite.body.fixedRotation = true;
     */
+
+    this.distText = game.add.text(50, 50, '', {font: "40px Arial", fill: "#ff0044"});
+    this.distText.fixedToCamera = true;
 };
 
 Shaft.update = function() {
+    this.distText.text = 'Energy: ' + this.energy;
 
+    //  only move when you click
+    if (game.input.mousePointer.isDown && this.energy > 0 && !this.connectionBroke)
+    {
+        //later this will depend on whether or not they clicked on a magnet
+        this.hooked = true;
+
+        //var speed = 2000 / Util.distanceBetween(player.body, this.clickSpot);
+        var speed = 1 / Util.distanceBetween(player.body, this.clickSpot);
+        speed *= 1000;
+
+        //console.log('Force: ' + 2000 * speed);
+
+        Util.accelerateToPoint(player, this.clickSpot, 2000 * speed);
+        if(Util.distanceBetween(player.body, this.clickSpot) < 50) {
+            player.body.force.x = 0;
+            player.body.force.y = 0;
+            player.body.velocity.x = 0;
+            player.body.velocity.y = 0;
+        }
+
+    } else if(this.energy <= 0) {
+        this.connectionBroke = true;
+        this.hooked = false;
+    } else {
+        this.hooked = false;
+    }
+
+    if(game.time.now > this.energyTicker) {
+
+        if(this.hooked === true) {
+            this.energy -= 2;
+        } else {
+            this.energy += 2;
+        }
+
+        this.energyTicker = game.time.now + 100;
+    }
+    if(this.energy > 100) this.energy = 100;
+    if(this.energy < 0) this.energy = 0;
 };
 
 Shaft.render = function() {
--- Src/Util.js
+++ Src/Util.js
@@ -1,10 +1,17 @@
 var Util = {
-	accelerateToPoint: function(player, x, y, speed) {
+	accelerateToPoint: function(player, destination, speed) {
 	    if (typeof speed === 'undefined') { speed = 60; }
 
 	    var angle = Math.atan2(destination.y - player.y, destination.x - player.x);
-	    //player.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
+	    //player.body.rotation = angle + game.math.degToRad(90); 
 	    player.body.force.x = Math.cos(angle) * speed;    
 	    player.body.force.y = Math.sin(angle) * speed;
+	},
+
+	distanceBetween: function(point1, point2) {
+		var distX = point1.x - point2.x;
+	    var distY = point1.y - point2.y;
+
+	    return Math.sqrt(distX * distX + distY * distY);
 	}
 };
